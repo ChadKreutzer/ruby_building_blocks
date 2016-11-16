694f5eacbfcfa1522a7ed33b987d90580a22b5e9
Chad Kreutzer <kanthalion@gmail.com>
2016-11-16 07:54:47 +0000

    complete substrings

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -3,29 +3,40 @@
 # argument. It should return a hash listing each substring (case insensitive)
 # that was found in the original string and how many times it was found.
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
 def substrings(sentence, substring_list)
-  match_array = []
-  match_hash = Hash.new(0)
-  substring_list.each do |s|
-    # if sentence.scan(/#{s}/i) != []
-    #   match_hash[s] += 1
-    # end
-    match_array.push(sentence.scan(/#{s}/i))
+  key_value_array = []
+  
+  keys(sentence, substring_list).each do |key_word|
+    key_value_array.push([key_word, key_word_count(sentence, key_word)])
   end
-  # match_hash
-  match_array.uniq
+  
+  Hash[key_value_array]
 end
 
-# if substrings("below", dictionary) == {"below"=>1, "low"=>1}
-#   puts true
-# else
-#   puts substrings("below", dictionary)
-# end
+def keys(template, match_array)
+  match_array.select{ |word| template =~ /#{word}/i }
+end
 
-if substrings("Howdy partner, sit down! How's it going?", dictionary) == {"down"=>1, "how"=>2, "howdy"=>1,"go"=>1, "going"=>1, "it"=>2, "i"=> 3, "own"=>1,"part"=>1,"partner"=>1,"sit"=>1}
+def key_word_count(template, key)
+  template.scan(/#{key}/i).length
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low",
+              "own","part","partner","sit"]
+
+test_string_1 = "below"
+test_string_2 = "Howdy partner, sit down! How's it going?"
+
+if substrings(test_string_1, dictionary) == {"below"=>1, "low"=>1}
   puts true
 else
-  puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+  puts substrings(test_string_1, dictionary)
+end
+
+if substrings(test_string_2, dictionary) == {"down"=>1, "how"=>2, "howdy"=>1,
+  "go"=>1, "going"=>1, "it"=>2, "i"=> 3, "own"=>1, "part"=>1, "partner"=>1,
+  "sit"=>1}
+  puts true
+else
+  puts substrings(test_string_2, dictionary)
 end
